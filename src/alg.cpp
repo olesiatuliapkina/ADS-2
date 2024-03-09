5e9ac67a5a9b36815acb93a5a231d7ae2e307ba9
olesiatuliapkina <151723481+olesiatuliapkina@users.noreply.github.com>
2024-03-09 11:00:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,40 @@
 
 
 double pown(double value, uint16_t n) {
-
+    uint64_t degreNum = value;
+    for (uint64_t i = 1; i < n; i++)
+        degreNum = degreNum*value;
+    return degreNum;
 }
 
 uint64_t fact(uint16_t n) {
-
+    if (n == 1)
+        return 1;
+    else
+        return n*fact(uint16_t n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-
+    double expRes = 1;
+    for (uint64_t i = 1; i <= count; i++)
+        expRes += calcItem(x, i);
+    return expRes;
 }
 
 double sinn(double x, uint16_t count) {
-
+    double sinRes = x;
+    for (uint64_t i = 2; i <= count; i++)
+        sinRes += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
+    return sinRes;
 }
 
 double cosn(double x, uint16_t count) {
-
+    double cosRes = 1;
+    for (uint64_t i = 2; i <= count; i++)
+        cosRes += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
+    return cosRes;
 }
